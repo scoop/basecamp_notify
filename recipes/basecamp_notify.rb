e3849cd44e63da9cbb707c8e9df63f6e99cb748a
Patrick Lenz <patrick+quadpro@limited-overload.de>
2008-04-04 23:32:03 +0200

    Initial version

---
+++ recipes/basecamp_notify.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/../vendor/basecamp'
+
+after 'deploy:symlink', 'basecamp:notify'
+
+namespace :basecamp do
+  set :basecamp_config do
+    YAML.load(File.open('config/basecamp.yml'))['basecamp']
+  end
+
+  set :api_wrapper do
+    Basecamp.new(basecamp_config['domain'], basecamp_config['user'], basecamp_config['password'])
+  end
+
+  desc 'Post a new message to Basecamp containing the commit messages between the previous and the current deploy'
+  task :notify do
+    api_wrapper.post_message basecamp_config['project_id'], {
+      :title => "Deploy: #{application} [#{current_revision[0..6]}]",
+      :body => grab_revision_log,
+      :category_id => basecamp_config['category_id']
+    }
+  end
+
+  desc 'Grab the revision log between the previous and the current deploy from the remote server'
+  task :grab_revision_log do
+    %x( git log --pretty=format:"* [%h,%an] %s" #{previous_revision}..#{current_revision} )
+  end
+end
